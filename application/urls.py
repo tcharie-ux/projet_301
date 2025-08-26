89ed7c26ec1c7172bba0ae12b0ec34149104f18b
TCHARIE-UX <lauretcharie@gmail.com>
2025-08-26 14:00:47 +0000

    Déplacement du fichier requirements.txt à la racine

--- application/urls.py
+++ application/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.urls import path, include
  
 urlpatterns=[
-
+path('', views.index, redirect_to_index),
 path('index/', views.index, name='index'),
 
 path('inscription/', views.inscript, name='inscription'),
